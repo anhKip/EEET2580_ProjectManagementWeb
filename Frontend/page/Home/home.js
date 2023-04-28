0ac7a4ff801b08e8b055903c3f8d5b2e4102aca7
LeTranTrongPhuoc <letrantrongphuoc@gmail.com>
2023-04-28 13:25:16 +0700

    Project list V1

---
+++ Frontend/page/Home/home.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('.menu-icon').click(function() {
+      $('.menu-container').toggleClass('open');
+    });
+  
+    // click event handler for create project card
+    $('.project-card-create').click(function(event) {
+      // prevent the default click behavior, which is to navigate to a new page
+      event.preventDefault();
+  
+      // show the gray overlay
+      $('.overlay').fadeIn();
+  
+      // show the create project box
+      $('.create-project-popup').fadeIn();
+    });
+  
+    // click event handler for close button and overlay
+    $('.close-btn, .overlay, .cancel-button').click(function() {
+      // hide the gray overlay and create project box
+      $('.overlay, .create-project-popup').fadeOut();
+    });
+  
+    // click event handler for submit button
+    $('.submit-button').click(function(event) {
+      // prevent the default submit behavior, which is to refresh the page
+      event.preventDefault();
+  
+      // hide the gray overlay and create project box
+      $('.overlay, .create-project-popup').fadeOut();
+    });
+  });
+  
