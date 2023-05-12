b833a9fd005bb58cfc36d85c29253435814fae22
Khoa10802 <s3884419@rmit.edu.vn>
2023-05-12 17:42:46 +0700

    navbar fix

--- Frontend/page/Account/account.js
+++ Frontend/page/Account/account.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
     $('.menu-icon').click(function() {
       $('.menu-container, .hide-menu').toggleClass('open');
     });
+
+    $('#show-hide-btn').click(function() {
+      if ($('#password-input').attr('type') == 'password') {
+        $('#show-hide-btn').html("<i class=\"fa-solid fa-eye-slash\"></i>");
+        $('#password-input').attr('type', 'text');
+      }
+      else {
+        $('#show-hide-btn').html("<i class=\"fa-solid fa-eye\"></i>");
+        $('#password-input').attr('type', 'password');
+      }
+    })
 })
