0b49553457c31aac08d34f311bf6cfc5fc9b098b
Khoa10802 <s3884419@rmit.edu.vn>
2023-05-12 16:52:23 +0700

    change to setting buttons

--- Frontend/page/Setting/setting.js
+++ Frontend/page/Setting/setting.js
@@ -3,34 +3,53 @@ $(document).ready(function() {
       $('.menu-container, .hide-menu').toggleClass('open');
     });
 
-    // click event handler for create project card
-    $('#add-contributor').click(function(event) {
-      // prevent the default click behavior, which is to navigate to a new page
+    // Pop-up form for changing project name
+    $('#change').click(function(event) {
       event.preventDefault();
   
-      // show the gray overlay
       $('.overlay').fadeIn();
   
-      // show the create project box
-      $('.add-form').fadeIn();
+      $('.name-change-form').fadeIn();
+
+      $('#name-input').val($('#project-name').text().trim());
     });
-  
-    // click event handler for close button and overlay
-    $('#cancel-button').click(function() {
-      // hide the gray overlay and create project box
-      $('.overlay, .add-form').fadeOut();
+
+    $('#cancel-change-button').click(function() {
+      $('.overlay, .name-change-form').fadeOut();
     });
-  
-    // click event handler for submit button
+
     $('#confirm-button').click(function(event) {
-      // prevent the default submit behavior, which is to refresh the page
       event.preventDefault();
   
-      // hide the gray overlay and create project box
+      $('.overlay, .name-change-form').fadeOut();
+    });
+
+
+    // Pop-up form for add contributors
+    $('#add-contributor').click(function(event) {
+      event.preventDefault();
+  
+      $('.overlay').fadeIn();
+  
+      $('.add-form').fadeIn();
+
+    });
+  
+    $('#cancel-button').click(function() {
       $('.overlay, .add-form').fadeOut();
     });
 
-    $('.contributor').append("<button type=\"button\" class=\"remove-btn\"> <i class=\"fa-solid fa-xmark\"></i> </button>")
+    $('#confirm-button').click(function(event) {
+      event.preventDefault();
+  
+      $('.overlay, .add-form').fadeOut();
+    });
+
+    // Add leave button for user account
+    $('.you').append("<button type=\"button\" class=\"remove-btn\"> Leave </button>")
+
+    // Add remove button for each contributors
+    $('.contributor').append("<button type=\"button\" class=\"remove-btn\"> Remove </button>")
 
     $('.remove-btn').click(function(event) {
       event.preventDefault();
@@ -39,40 +58,4 @@ $(document).ready(function() {
       
     })
 })
-
-const s = document.getElementById("save");
-const c = document.getElementById("cancel");
-const ch = document.getElementById("change");
-const n = document.getElementById("project-name");
-var nameBuffer
-
-function buttonChange() {
-  nameBuffer = n.value
-  ch.style.display = "none"
-  s.style.display = "block"
-  c.style.display = "block"
-
-  n.removeAttribute("readonly")
-  n.style.outlineStyle = "solid"
-  n.style.outlineWidth = "medium"
-  n.style.outlineColor = "#ABABAB"
-}
-
-function buttonCancel() {
-  ch.style.display = "block"
-  s.style.display = "none"
-  c.style.display = "none"
-
-  n.setAttribute("readonly", "true")
-  n.style.outline = "none"
-  n.value = nameBuffer
-}
-
-function buttonSave() {
-
-}
-
-document.getElementById("change").addEventListener("click", buttonChange)
-document.getElementById("cancel").addEventListener("click", buttonCancel)
-document.getElementById("save").addEventListener("click", buttonSave)
 
