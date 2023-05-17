ae7e32a0974eb6a7313e4101157fe0f47277d6e3
LeTranTrongPhuoc <letrantrongphuoc@gmail.com>
2023-05-17 20:02:58 +0700

    fixed the password do not match

--- Frontend/page/Sign up/signup.js
+++ Frontend/page/Sign up/signup.js
@@ -1,42 +1,51 @@
 document.addEventListener("DOMContentLoaded", function() {
+    let pass1 = document.getElementById("password");
+    let pass2 = document.getElementById("password-confirm");
+    let passwordError = document.getElementById("password-error");
+  
+    pass1.addEventListener("input", validatePassword);
+    pass2.addEventListener("input", validatePassword);
+  
+    function validatePassword() {
+      if (pass1.value !== pass2.value) {
+        passwordError.style.display = "block";
+        pass1.classList.add("is-invalid");
+        pass2.classList.add("is-invalid");
+      } else {
+        passwordError.style.display = "none";
+        pass1.classList.remove("is-invalid");
+        pass2.classList.remove("is-invalid");
+      }
+    }
+  
     document.querySelector("form").addEventListener("submit", function(event) {
       event.preventDefault();
   
-      let pass1 = document.getElementById("password");
-      let pass2 = document.getElementById("password-confirm");
-      let passwordError = document.getElementById("password-error");
+      if (pass1.value === pass2.value) {
+        let url = 'http://localhost:8080/api/auth/signup';
+        let inputs = {
+          email: document.querySelector('#email').value,
+          username: document.querySelector("#username").value,
+          password: pass1.value
+        };
   
-      if (pass1.value !== pass2.value) {
-          passwordError.style.display = "block";  // Show the error message
-          pass1.value = "";
-          pass2.value = "";
-      } else {
-          passwordError.style.display = "none";   // Hide the error message
+        console.log(inputs);
   
-          let url = 'http://localhost:8080/api/auth/signup';
-          let inputs = {
-              email: document.querySelector('#email').value,
-              username: document.querySelector("#username").value,
-              password: document.querySelector('#password').value
-          };
-  
-          console.log(inputs);
-  
-          fetch(url, {
-              method: "POST",
-              headers: {
-                  "Content-Type": "application/json"
-              },
-              body: JSON.stringify(inputs)
-          })
-          .then(response => response.json())
-          .then(json => {
-              console.log(json.message);
-              window.location.assign('../Login/login.html');
-          })
-          .catch(e => {
-              console.log(e);
-          });
+        fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(inputs)
+        })
+        .then(response => response.json())
+        .then(json => {
+          console.log(json.message);
+          window.location.assign('../Login/login.html');
+        })
+        .catch(e => {
+          console.log(e);
+        });
       }
     });
   });
