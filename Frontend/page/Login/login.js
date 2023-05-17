88c1ff1e9d1650634f866d7fb785f9a30d0501aa
Khoa10802 <s3884419@rmit.edu.vn>
2023-05-17 18:04:24 +0700

    signup&signin

--- Frontend/page/Login/login.js
+++ Frontend/page/Login/login.js
@@ -1,47 +1,24 @@
 /**
- *
- * @param {string} cname Token name
- * @param {string} cvalue Token value
+ * 
+ * @param {string} c_name Token name
+ * @param {string} c_value Token value
  * @param {float} expire Expiration Time in Hour
  */
-function setTokenCookies(cname, cvalue, expire) {
+function setTokenCookies(cname, c_value, expire) {
     const d = new Date();
-    d.setTime(d.getTime() + expire * 60 * 60 * 1000);
-    let expires = " expires=" + d.toUTCString();
-    document.cookie =
-        cname +
-        "=" +
-        cvalue +
-        ";" +
-        expires +
-        "; path=/" +
-        "; SameSite=None" +
-        "; Secure";
-    // document.cookie = cname + '=' + cvalue + ';' + expires + '; path=/' + '; SameSite=None' + '; Secure' + '; HttpOnly';
-}
-
-function setIdToken(cname, cvalue, expire) {
-    const d = new Date();
-    d.setTime(d.getTime() + expire * 60 * 60 * 1000);
-    let expires = " expires=" + d.toUTCString();
-    document.cookie =
-        cname +
-        "=" +
-        cvalue +
-        ";" +
-        expires +
-        "; path=/" +
-        "; SameSite=None" +
-        "; Secure";
+    d.setTime(d.getTime() + (expire*60*60*1000));
+    let expires = " expires="+ d.toUTCString();
+    document.cookie = cname + '=' + c_value + ';' + expires + '; path=/' + '; SameSite=None' + '; Secure';
+    // document.cookie = cname + '=' + c_value + ';' + expires + '; path=/' + '; SameSite=None' + '; Secure' + '; HttpOnly';
 }
 
 function getTokenCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(";");
-    for (let i = 0; i < ca.length; i++) {
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
         let c = ca[i];
-        while (c.charAt(0) == " ") {
+        while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {
@@ -51,48 +28,37 @@ function getTokenCookie(cname) {
     return "";
 }
 
-// setTokenCookies('name2', 'Long', 0.05)
-// console.log(getTokenCookie("name2"));
+document.getElementById("login-btn").addEventListener("click", function(event) {
+    event.preventDefault()
 
-document
-    .querySelector("#login-btn")
-    .addEventListener("click", async function () {
-        console.log("button pressed");
+    let url = 'http://localhost:8080/api/auth/signin'
 
-        let url = "http://localhost:8080/api/auth/signin";
+    let inputs = {
+        username: document.querySelector('#username').value,
+        password: document.querySelector('#password').value
+    }
 
-        const username = await document.querySelector("#username").value;
-        const password = await document.querySelector("#password").value;
+    fetch (url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(inputs)
+    })
+    .then(response => response.json())
+    .then(json => {
+        // console.log(json);
+        if (json.userId >= 0) {
+            console.log("Log in successfully")
+            window.location.assign('../Home/home.html' + '?u=' + json.userId)
+        }
+        else {
+            console.log("Log in fails")
+        }
+    })
+    .catch(e => {
+        console.log(e)
+    })
+})
 
-        let inputs = {
-            username: username,
-            password: password,
-        };
 
-        console.log(inputs);
-        console.log(JSON.stringify(inputs));
-
-        fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(inputs),
-        })
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json);
-                // console.log('userId is' + json.userId);
-                if (json.userId >= 0) {
-                    console.log("Log in successfully");
-                    // setTokenCookies('accessToken', json.accessToken, 0.1)
-                    // setTokenCookies('refreshToken', json.refreshToken, 0.1)
-                    // window.location.assign('../Home/home.html')
-                } else {
-                    console.log("Log in fails");
-                }
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-    });
