3e881a4fa6d9211c2f65e4d8ee2602e68a9fa7d4
Khoa10802 <s3884419@rmit.edu.vn>
2023-05-18 18:17:10 +0700

    change-password

--- Frontend/page/Account/account.js
+++ Frontend/page/Account/account.js
@@ -1,7 +1,10 @@
 import { pageLoader, addWrapper } from "../../functions/pageLoader.js";
+import { getIdCookie, reLog } from "../../functions/authentications.js";
 
 addWrapper()
 pageLoader()
+
+const userId = getIdCookie("userId");
 
 $(document).ready(function () {
     $(".menu-icon").click(function () {
@@ -27,6 +30,40 @@ $(document).ready(function () {
         }
     });
 });
+
+document.getElementById("save-btn").addEventListener("click", changePassword)
+
+function changePassword(event) {
+    event.preventDefault();
+
+    const url = "http://localhost:8080/api/auth/change-password"
+
+    const password = document.querySelector("#password-input").value;
+
+    let data = {
+        'id': parseInt(userId),
+        'newPassword': password,
+    }
+
+    console.log(data);
+    console.log(JSON.stringify(data));
+    console.log(typeof JSON.stringify(data));
+
+    fetch (url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json);
+    })
+    .catch((e) => {
+        console.log(e)
+    })
+}
 
 document.querySelector(".fa-rotate").addEventListener("click", function () {
     location.reload();
